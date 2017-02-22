940d95380d375c685338e6d3e67d7d6756c9a732
jurandir dacol junior <jurandir.junior@mobly.com.br>
2017-02-22 19:34:18 -0300

    [dacolera] several abstract improvements

--- features/bootstrap/Context/AbstractContext.php
+++ features/bootstrap/Context/AbstractContext.php
@@ -18,36 +18,98 @@ abstract class AbstractContext implements Context
      */
     protected $minkContext;
 
+    protected $baseUrl;
+
+    protected $secureBaseUrl;
+
+    public static $sharedUrl;
+
     /**
      * @BeforeScenario
      */
     public function gatherContexts(BeforeScenarioScope $scope)
     {
-        switch ($_SERVER['APPLICATION_ENV']) {
-        case 'live':
-            $baseUrl = 'http://www.mobly.com.br';
-            break;
-        case 'staging':
-            $baseUrl = 'http://alice-staging.mobly.com.br';
-            break;
-        case 'dev':
-            $baseUrl = 'http://alice.mobly.dev';
-            break;
-        default:
-            throw new \RuntimeException('Variavel de ambiente APPLICATION_ENV possui valor invalido');
-        }
-
-        $environment = $scope->getEnvironment();
+        $environment = $this->getEnvironment($scope);
         $this->minkContext = $environment->getContext('Behat\MinkExtension\Context\MinkContext');
-        $this->minkContext->setMinkParameter('base_url', $baseUrl);
+        $this->minkContext->setMinkParameter('base_url', $this->baseUrl);
+
+        static::$sharedUrl = $this->minkContext->getMinkParameter('base_url');
 
         if ($this->minkContext->getSession()->getDriver() instanceof \Behat\Mink\Driver\Selenium2Driver) {
-            $this->minkContext->getSession()->resizeWindow(1366, 768, 'current');
+            $this->minkContext->getSession()->resizeWindow(1920, 1080, 'current');
         }
     }
 
+    /**
+     * @return \Behat\Mink\Element\DocumentElement
+     */
     protected function getPage()
     {
         return $this->minkContext->getSession()->getPage();
+    }
+
+    /**
+     * @param BeforeScenarioScope $scope
+     * @return \Behat\Testwork\Environment\Environment
+     */
+    protected function getEnvironment(BeforeScenarioScope $scope)
+    {
+        switch ($_SERVER['APPLICATION_ENV']) {
+            case 'live':
+                $this->baseUrl = 'http://www.mobly.com.br';
+                $this->secureBaseUrl = 'https://secure.mobly.com.br';
+                break;
+            case 'staging':
+                $this->baseUrl = 'http://rocket:rock4me@alice-staging01.mobly.com.br';
+                $this->secureBaseUrl = 'https://rocket:rock4me@staging01-secure.mobly.com.br';
+                break;
+            case 'dev':
+                $this->baseUrl = 'http://alice.mobly.dev';
+                $this->secureBaseUrl = 'https://alice-secure.mobly.dev';
+                break;
+            default:
+                throw new \RuntimeException('Variavel de ambiente APPLICATION_ENV possui valor invalido');
+        }
+
+        return $scope->getEnvironment();
+    }
+
+    /**
+     * @return $this
+     */
+    protected function setSslBaseUrl()
+    {
+        $this->minkContext->setMinkParameter('base_url', $this->secureBaseUrl);
+
+        static::$sharedUrl = $this->minkContext->getMinkParameter('base_url');
+
+        return $this;
+    }
+
+    /**
+     * @param $version
+     * @return $this|null
+     */
+    protected function selectMultiStagingServerVersion($version)
+    {
+        if ($_SERVER['APPLICATION_ENV'] != 'staging') {
+            //don't bother in this case
+            return null;
+        }
+        if (!in_array((int)$version, [1,2,3,4,5])) {
+            //don't bother either
+            return null;
+        }
+
+        $host = preg_replace('/(staging)/', '${1}0'. $version, $this->minkContext->getMinkParameter('base_url'));
+
+        $this->minkContext->setMinkParameter(
+            'base_url',
+            $host
+        );
+
+        static::$sharedUrl = $this->minkContext->getMinkParameter('base_url');
+
+        return $this;
     }
 }
--- features/bootstrap/Page/AbstractPage.php
+++ features/bootstrap/Page/AbstractPage.php
@@ -3,9 +3,59 @@
 namespace Page;
 
 use SensioLabs\Behat\PageObjectExtension\PageObject\Page;
+use Context\AbstractContext;
+use Behat\Mink\Session;
+use SensioLabs\Behat\PageObjectExtension\PageObject\Factory;
 
 abstract class AbstractPage extends Page
 {
     use FakerTrait;
 
+    /**
+     * @param Session $session
+     * @param Factory $factory
+     * @param array   $parameters
+     */
+    public function __construct(Session $session, Factory $factory, array $parameters = array())
+    {
+        parent::__construct($session, $factory, ['base_url' => AbstractContext::$sharedUrl]);
+    }
+
+    /**
+     * expect [fakerType =>[params]] | fakerType
+     * @param null $value
+     * @param string|array $fakerType
+     * @return mixed|null
+     */
+    protected function setValueOrUseFaker($value = null, $fakerType = null)
+    {
+        if (null === $value) {
+            if (is_array($fakerType)) {
+                return call_user_func_array([$this->getFaker(), key($fakerType)], $fakerType[key($fakerType)]);
+            }
+
+            return $this->getFaker()->$fakerType;
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param $locator
+     * @param $value
+     */
+    protected function fillFieldWithMask($locator, $value)
+    {
+        $element = $this->find('css', $locator);
+
+        if (!is_null($element)) {
+            $element->click();
+            $element->setValue($value);
+        }
+    }
+
+    protected function verifyUrl(array $urlParameters = array())
+    {
+        // do nothing thank GOD !
+    }
 }
